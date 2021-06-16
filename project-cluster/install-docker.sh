2aa6b4767c4a97f974691c8e52fb16c0387deea8
xiejinglei <2301089907@qq.com>
2021-06-16 20:34:04 +0800

    add cluster

---
+++ project-cluster/install-docker.sh
@@ -0,0 +1,16 @@
+# Install dependencies
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
---
+++ project-cluster/manual.md
@@ -0,0 +1,8 @@
+# Project cluster setup
+## General info
+1. Machine info: https://focs.ji.sjtu.edu.cn/git/TAs/ve472/issues/5
+2. Docker configurations: https://github.com/tc-imba/hadoop-cluster
+
+## Steps
+### Starting with servers
+- Install docker for each server
