b6a49d21a1914a1718d6c824ac675ec5b716666e
xiejinglei <2301089907@qq.com>
2021-06-17 22:41:25 +0800

    update script

--- project-cluster/install-docker.sh
+++ project-cluster/install-docker.sh
@@ -1,9 +1,10 @@
 # Install dependencies
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gpg \
     gnupg \
     lsb-release
 
@@ -13,4 +14,4 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
