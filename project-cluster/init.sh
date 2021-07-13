f58f094e8b33cdfe76b1a8de0f5a8803976f1c3b
xiejinglei <2301089907@qq.com>
2021-07-13 19:07:03 +0800

    remove file

--- project-cluster/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-
-mkdir /ta-su2020/
-cd /ta-su2020
-git clone https://github.com/xiejinglei/hadoop-eco.git
-
-bash /ta-su2020/hadoop-eco/project-cluster/install-docker.sh
